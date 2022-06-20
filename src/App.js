dd41e0a6a0524c6da17ece7de98099704aea5b35
Prateek Varma <prtk.varma@gmail.com>
2022-06-20 16:15:20 -0600

    Test commit, to check useEffect() Hook's config with empty [] as 2nd param

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -16,10 +17,13 @@ const items = [
   }
 ]
 
-export default () => {
+ const App = () => {
   return (
     <div>
+      <Search />
       <Accordion items={items} />
     </div>
   );
 };
+
+export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,30 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
 
-    // setTerm() logic goes here
-    
-    return(
-        <div>
-            <div className="ui form">
-            <div className="field">
-                <label>Enter Search Term: </label>
-                <input className="input" value={term} onChange={(e) => setTerm(e.target.value)} />
-            </div>
-            </div>
+  //Testing useEffect() : Below, the console will be called every time the input field is typed, hence the component will be rendered every time, and therefore the console will be logged each time.
+  console.log("Called every time you type.");
+
+  //Below the useEffect has an empty [] as 2nd param, so it will render only once
+  useEffect(() => {
+    console.log("Just called on initialization");
+  }, []);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term: </label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
