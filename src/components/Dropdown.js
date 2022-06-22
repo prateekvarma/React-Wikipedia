d2c302ddf1ac5973fc54ddc05633211ce5fbd86f
Prateek Varma <prtk.varma@gmail.com>
2022-06-22 14:32:46 -0600

    used useEffect and useRef to close dropdown when clicked in any part of the HTML body

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = (props) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  //Below,the useEffect is configured to run only on initialization, but it will still execute on each click, bcoz it's expected behavior for addEventListener to continue.
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      if (ref.current.contains(event.target)) {
+        //if dropdown is clicked, do not execute setOpen()
+        return;
+      }
+      //Below, setting state var as false, because with this, the dropdown will now close when any part of the 'body' is clicked
+      setOpen(false);
+    });
+  }, []);
+
   const renderedOptions = props.options.map((option) => {
     return (
       <div
@@ -14,7 +28,7 @@ const Dropdown = (props) => {
     );
   });
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a color</label>
         <div
@@ -23,7 +37,7 @@ const Dropdown = (props) => {
         >
           <i className="dropdown icon"></i>
           <div className="text">{props.selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
             {renderedOptions}
           </div>
         </div>
