aebc8152ceb960feda64f1cebe1781d133162f8d
Prateek Varma <prtk.varma@gmail.com>
2022-06-22 12:40:36 -0600

    used State to track the dropdown's click, and displayed CSS classes using ternary operator to hide/show slected item from dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = (props) => {
-    const renderedOptions = props.options.map((option) => {
-        return (
-            <div key={option.value} className="item" onClick={() => props.onSelectedChange(option)}>{option.label}</div>
-        )
-    })
+  const [open, setOpen] = useState(false);
+  const renderedOptions = props.options.map((option) => {
     return (
-        <div className="ui form">
-            <div className="field">
-                <label className="label">Select a color</label>
-                <div className="ui selection dropdown visible active">
-                    <i className="dropdown icon"></i>
-                    <div className="text">{props.selected.label}</div>
-                    <div className="menu visible transition">
-                        {renderedOptions}
-                    </div>
-                </div>
-            </div>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => props.onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{props.selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
