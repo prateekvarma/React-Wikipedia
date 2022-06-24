f9d4c45007f518deed229fa0518ee5808b225f32
Prateek Varma <prtk.varma@gmail.com>
2022-06-23 20:00:08 -0600

    used state to store value of translated text, and used async/await inside useEffect by defining a new function inside

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = (props) => {
+  const [translated, setTranslated] = useState("");
+
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-        params: {
+    const doTranslation = async () => {
+      const res = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
             q: props.text,
             target: props.language.value,
-            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            key: "XXX",
+          },
         }
-    })
+      );
+      setTranslated(res.data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [props.language, props.text]);
 
-  return <div>Convert</div>;
+  return <div>{translated}</div>;
 };
 
 export default Convert;
