904df18988b6e80e4db9af8b52861de316524182
Prateek Varma <prtk.varma@gmail.com>
2022-06-23 19:30:43 -0600

    installed axios and constructed query string for Google translate API. Result Can be verified from Network tab > XHR request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 
 const Convert = (props) => {
   useEffect(() => {
-    console.log("useEffect func triggered!");
+    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+            q: props.text,
+            target: props.language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+    })
   }, [props.language, props.text]);
 
   return <div>Convert</div>;
